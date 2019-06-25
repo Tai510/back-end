55b6463dda58742102d67a3c36740c38bbe25216
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-06-25 12:40:42 -0700

    signup change

--- auth/signUp-router.js
+++ auth/signUp-router.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const tokenService = require('../auth/token-service.js');
 
 const Users = require('../users/users-model.js')
